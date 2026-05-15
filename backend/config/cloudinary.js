fd9f0e91b0c2b83d6240ac57099d1abdd23f6bd1
arjungk07 <arjun.gk10g2021.22@gmail.com>
2026-05-15 10:58:10 +0530

    cloudinary update

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,12 +1,15 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import multer from 'multer';
+import dotenv from "dotenv";
+dotenv.config();
+
 
 // Configuration
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const storage = new CloudinaryStorage({
